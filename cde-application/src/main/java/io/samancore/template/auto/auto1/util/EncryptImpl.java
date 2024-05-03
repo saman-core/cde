9a1c50a8d9efe699be89d6f281b9bfae1759e723
cfernandez <Ycuervo.659>
2024-05-03 11:05:54 -0400

    changes

--- cde-application/src/main/java/io/samancore/template/auto/auto1/util/EncryptImpl.java
+++ cde-application/src/main/java/io/samancore/template/auto/auto1/util/EncryptImpl.java
@@ -3,15 +3,22 @@ package io.samancore.template.auto.auto1.util;
 import io.samancore.common.transformer.Encrypt;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 @ApplicationScoped
 public class EncryptImpl implements Encrypt {
+
+    private final static Charset charset = StandardCharsets.UTF_8;
+
     @Override
-    public Object encrypt(Object o) {
-        return o;
+    public byte[] encrypt(Object o) {
+        return charset.encode((String)o).array();
     }
 
     @Override
-    public Object decrypt(Object o) {
-        return o;
+    public String decrypt(byte[] o) {
+        return charset.decode(ByteBuffer.wrap(o)).toString();
     }
 }
