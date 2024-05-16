dd23e8747222f1df562fd1cd73300852d8a901b4
Hernael <hperer@movealong.tech>
2024-05-15 23:31:31 -0400

    test for rsa encrypt

--- cde-application/src/main/java/io/samancore/template/auto/auto1/util/EncryptImpl.java
+++ cde-application/src/main/java/io/samancore/template/auto/auto1/util/EncryptImpl.java
@@ -9,6 +9,7 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.services.kms.KmsClient;
 import software.amazon.awssdk.services.kms.model.DecryptResponse;
+import software.amazon.awssdk.services.kms.model.EncryptionAlgorithmSpec;
 
 @ApplicationScoped
 public class EncryptImpl implements Encrypt {
@@ -22,7 +23,12 @@ public class EncryptImpl implements Encrypt {
     @Override
     public byte[] encrypt(Object var1) {
         String data = String.valueOf(var1);
-        SdkBytes encryptedBytes = kms.encrypt(req -> req.keyId(keyArn).plaintext(SdkBytes.fromUtf8String(data))).ciphertextBlob();
+        SdkBytes encryptedBytes = kms.encrypt(req ->
+                req
+                        .keyId(keyArn)
+                        .encryptionAlgorithm(EncryptionAlgorithmSpec.RSAES_OAEP_SHA_1)
+                        .plaintext(SdkBytes.fromUtf8String(data))
+        ).ciphertextBlob();
 
         return Base64.getEncoder().encode(encryptedBytes.asByteArray());
     }
@@ -30,7 +36,12 @@ public class EncryptImpl implements Encrypt {
     @Override
     public String decrypt(byte[] data) {
         SdkBytes encryptedData = SdkBytes.fromByteArray(Base64.getDecoder().decode(data));
-        DecryptResponse decrypted = kms.decrypt(req -> req.keyId(keyArn).ciphertextBlob(encryptedData));
+        DecryptResponse decrypted = kms.decrypt(req ->
+                req
+                        .keyId(keyArn)
+                        .encryptionAlgorithm(EncryptionAlgorithmSpec.RSAES_OAEP_SHA_1)
+                        .ciphertextBlob(encryptedData)
+        );
 
         return decrypted.plaintext().asUtf8String();
     }
